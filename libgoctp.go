07f7892a0ce6f5ad116d7b7977f258f61d2dd32d
Ye Shengnan <thinkphoebe@gmail.com>
2020-02-03 23:11:41 +0800

    update for 'go mod vendor' api files copy

--- libgoctp.go
+++ libgoctp.go
@@ -1,6 +1,10 @@
 // +build linux,cgo windows,cgo
 package goctp
 
+import (
+	_ "github.com/thinkphoebe/ctp/api/6.3.15_linux64" // for go mod vendor
+)
+
 /*
 #cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/6.3.15_linux64 -Wl,-rpath,${SRCDIR}/api/6.3.15_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
 #cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/6.3.15_linux64
