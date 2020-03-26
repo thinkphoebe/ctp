af93c84f3db3739595d1a23917d1a564f450d691
Ye Shengnan <thinkphoebe@gmail.com>
2020-03-26 17:40:53 +0800

    add files

--- setup.py
+++ setup.py
@@ -21,14 +21,15 @@ if platform.system() == 'Linux':
     optional['runtime_library_dirs'] = ['./']
     optional['include_dirs']=['./api/6.3.15_linux64']
     optional['library_dirs']=['./api/6.3.15_linux64']
-#    depend_dynamics = ['libthostmduserapi.so', 'libthosttraderapi.so']
+    depend_dynamics = ['api/6.3.15_linux64/libthostmduserapi_se.so', 'api/6.3.15_linux64/libthosttraderapi_se.so']
 if platform.system() == 'Windows':
     optional['include_dirs'] = ['./api/6.3.15_win32']
     optional['library_dirs'] = ['./api/6.3.15_win32']
+    depend_dynamics = ['api/6.3.15_win32/thostmduserapi_se.dll', 'api/6.3.15_win32/thosttraderapi_se.dll']
     if '64 bit' in platform.python_compiler():
         optional['include_dirs'] = ['./api/6.3.15_win64']
         optional['library_dirs'] = ['./api/6.3.15_win64']
-#    depend_dynamics = ['thostmduserapi.dll', 'thosttraderapi.dll']
+        depend_dynamics = ['api/6.3.15_win64/thostmduserapi_se.dll', 'api/6.3.15_win64/thosttraderapi_se.dll']
 argments = dict(name='PyCTP',
                 sources=sources,
                 language='c++',
@@ -37,19 +38,17 @@ argments = dict(name='PyCTP',
 argments.update(optional)
 
 setup(name='PyCTP',
-      version='1.0.1',
-      description='CTP for Python',
-      long_description='CTP v6.3.15 for Python',
-      author='Shi Zhuolin',
-      author_email='shizhuolin@hotmail.com',
-      url='http://www.pyctp.org/',
-      keywords=['ctp','futures','stock'],
-      license='LGPL-3.0',
-      platforms=['linux-x86_64','win32','win-amd64'],
-      ext_modules=[Extension(**argments)],
-      scripts=['./pyctp/test_PyCTP.py'],
-#      #data_files=[('lib64', depend_dynamics)],
-#      #packages=['PyCTP'],
-#      #package_dir={'PyCTP':optional['library_dirs'][0]},
-#      #package_data={'PyCTP':depend_dynamics}
+        version='1.0.1',
+        description='CTP for Python',
+        long_description='CTP v6.3.15 for Python',
+        author='Shi Zhuolin',
+        author_email='shizhuolin@hotmail.com',
+        url='http://www.pyctp.org/',
+        keywords=['ctp','futures','stock'],
+        license='LGPL-3.0',
+        platforms=['linux-x86_64','win32','win-amd64'],
+        ext_modules=[Extension(**argments)],
+        scripts=['./pyctp/test_PyCTP.py'],
+
+        #data_files=[('lib', depend_dynamics)],
       )
