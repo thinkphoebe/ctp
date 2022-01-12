a23c95a34c8cd9d1dd19075d9b6592e92576b52e
Ye Shengnan <thinkphoebe@gmail.com>
2022-01-12 23:22:14 +0800

    fix 6.6.1_P1

--- libgoctp.go
+++ libgoctp.go
@@ -2,16 +2,13 @@
 package goctp
 
 import (
-	//_ "github.com/thinkphoebe/ctp/api/6.3.15_linux64" // for go mod vendor
-	//_ "github.com/thinkphoebe/ctp/api/6.3.16_linux64" // for go mod vendor
-	//_ "github.com/thinkphoebe/ctp/api/6.3.19_P1_linux64" // for go mod vendor
 	_ "github.com/thinkphoebe/ctp/api/6.6.1_P1_linux64" // for go mod vendor
 )
 
 /*
-#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/6.3.15_linux64 -Wl,-rpath,${SRCDIR}/api/6.3.15_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
-#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/6.3.15_linux64
-#cgo windows LDFLAGS: -fPIC -L${SRCDIR}/api/6.3.15_windows64 -Wl,-rpath,${SRCDIR}/api/6.3.15_windows64 ${SRCDIR}/6.3.15_windows64/thostmduserapi.lib ${SRCDIR}/api/6.3.15_windows64/thosttraderapi.lib -lthostmduserapi -lthosttraderapi
-#cgo windows CPPFLAGS: -fPIC -I${SRCDIR}/api/6.3.15_windows64 -DISLIB -DWIN32 -DLIB_MD_API_EXPORT -DLIB_TRADER_API_EXPORT
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/6.6.1_P1_linux64 -Wl,-rpath,${SRCDIR}/api/6.6.1_P1_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
+#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/6.6.1_P1_linux64
+#cgo windows LDFLAGS: -fPIC -L${SRCDIR}/api/6.6.1_P1_windows64 -Wl,-rpath,${SRCDIR}/api/6.6.1_P1_windows64 ${SRCDIR}/6.6.1_P1_windows64/thostmduserapi.lib ${SRCDIR}/api/6.6.1_P1_windows64/thosttraderapi.lib -lthostmduserapi -lthosttraderapi
+#cgo windows CPPFLAGS: -fPIC -I${SRCDIR}/api/6.6.1_P1_windows64 -DISLIB -DWIN32 -DLIB_MD_API_EXPORT -DLIB_TRADER_API_EXPORT
 */
 import "C"
